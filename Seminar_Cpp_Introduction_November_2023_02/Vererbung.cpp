73b298c05f7aac2fe100636f95688045f8ebdc4e
Peter Loos <peter.loos@gmx.de>
2023-11-30 15:14:41 +0100

    Stand 15:30

--- Seminar_Cpp_Introduction_November_2023_02/Vererbung.cpp
+++ Seminar_Cpp_Introduction_November_2023_02/Vererbung.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
-class Rectangle
+// UI für graphische Figuren
+
+class IRectangle
+{
+public:
+    virtual void draw() = 0;
+    virtual void eraseBackground() = 0;
+    virtual void drawBorder() = 0;
+};
+
+// ====================================================
+
+class Rectangle : public IRectangle  // , ICloneable, IComparable
 {
 private:
     int m_x;
@@ -26,19 +38,29 @@ public:
     int getHeight() { return m_height; }
 
     // public interface
-    //void eraseBackground()
-    //{
-    //    std::cout << "  Rectangle::eraseBackground" << std::endl;
-    //}
+    // KEIN VIRTUAL
+    // KEIN VERDECKEN
+
+    void eraseBackground()
+    {
+        std::cout << "  Rectangle::ERASEBackground" << std::endl;
+    }
+
+    virtual void drawBorder() = 0;   // abstract // keine Realisierung
+
 
 public:
-    void draw() {
+    virtual void draw() {
+
+        // Grundlegendes
 
         std::cout
-            << "Rectangle::draw [x=" << m_x
+            << "  Rectangle::draw [x=" << m_x
             << ", y=" << m_y << "]" << std::endl;
 
-        // eraseBackground();
+        eraseBackground();
+
+        drawBorder();
     }
 };
 
@@ -52,32 +74,109 @@ public:
         : Rectangle (x, y, width, height), m_color (color)
     {}
 
-    // Verdecken
     void draw() {
         std::cout << "ColoredRectangle::draw" << std::endl;
 
-        std::cout
-            << "  [x=" << getX()
-            << ", y=" << getY() << "]" << std::endl;
-
         //std::cout
-        //    << "  [x=" << m_x
-        //    << ", y=" << m_y << "]" << std::endl;
+        //    << "  [x=" << getX()
+        //    << ", y=" << getY() << "]" << std::endl;
 
         // Wiederverwendung:
-       // Rectangle::draw();
+        Rectangle::draw();
+
+        std::cout << "  ColoredRectangle malt seinen farbigen Anteil" << std::endl;
+    }
+
+    void drawBorder()
+    {
+        std::cout << "  ColoredRectangle malt seinen RAND" << std::endl;
+    }
+};
+
+class TransparentRectangle : public Rectangle
+{
+private:
+    double m_opaque;  // representing some transparency model
+
+public:
+    // c'tor(s)
+    TransparentRectangle()
+        : TransparentRectangle(0, 0, 0, 0, 0.0)
+    {}
+
+    TransparentRectangle(int x, int y, int width, int height, double transparency)
+        : Rectangle(x, y, width, height), m_opaque(transparency)
+    {}
+
+    void draw() override {
+        std::cout << "TransparentRectangle::draw" << std::endl;
+
+        // Wiederverwendung:
+        Rectangle::draw();
+
+        std::cout << "  TransparentRectangle malt seinen farbigen Anteil" << std::endl;
+    }
+
+    void drawBorder()
+    {
+        std::cout << "  TransparentRectangle malt seinen RAND" << std::endl;
     }
 };
 
 
-void test_inheritance()
-{
-    Rectangle r(1, 1, 20, 40);
-    r.draw();
 
-    // AUßerhalb der Klasse
-    // r.m_x = -1;   // NEIN, dies soll NIEMALS GEHEN
+void test_inheritance_01()
+{
+    //Rectangle r(1, 1, 20, 40);
+    //r.draw();
 
     ColoredRectangle cr(2, 2, 30, 60, 88);
     cr.draw();
 }
+
+void test_inheritance_02()
+{
+    Rectangle* r;
+
+    ColoredRectangle cr(2, 2, 30, 60, 88);
+
+    r = & cr;  // is-a Beziehung
+
+    r->draw();  // A) r ist Pointer auf Rectangle:
+                //    ==> Aufruf von Rectangle::draw
+                // B) r zeigt auf ein Objekt des Types ColoredRectangle:
+                //    ==> Aufruf von ColoredRectangle::draw
+}
+
+void test_inheritance_03()
+{
+    ColoredRectangle cr1(2, 2, 30, 60, 88);
+    ColoredRectangle cr2(1, 1, 20, 20, 88);
+
+    TransparentRectangle tr1(3, 3, 60, 80, 44.44);
+    TransparentRectangle tr2(4, 4, 60, 90, 44.44);
+    TransparentRectangle tr3(5, 5, 60, 70, 44.44);
+
+    // =====================================
+
+    IRectangle* rectangles[5]{ &cr1, &tr1, &cr2, &tr2, &tr3 };
+
+    for (int i = 0; i < 5; ++i) {
+
+        rectangles[i]->draw();
+
+        //if (rectangles[i] ist Typ ColoredRectangle) {
+        //    Methode von ColoredRectangle;
+        //}
+        //else   if (rectangles[i] ist Typ TransparentRectangle) {
+        //    Methode von TransparentRectangle;
+        //}
+    }
+}
+
+
+void test_inheritance()
+{
+    test_inheritance_03();
+}
+
